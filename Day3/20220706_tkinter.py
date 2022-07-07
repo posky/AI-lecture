ceb6fecceb20a836f86be9815517ed97a4f27673
posky <pjm5810@gmail.com>
2022-07-07 13:02:06 +0900

    3일차

--- Day3/20220706_tkinter.py
+++ Day3/20220706_tkinter.py
@@ -2,10 +2,6 @@ from tkinter import *
 import pandas as pd
 import os
 
-def click():
-    pass
-
-
 global dat
 
 print(os.getcwd())
@@ -16,6 +12,17 @@ print(os.getcwd())
 dat = pd.read_excel('dic_excel.xlsx')
 print(dat.shape, dat)
 print()
+
+def click():
+    word = entry.get()
+    output.delete(0.0, END)     # 내용 지우기 (text)
+
+    try:
+        def_word = dat.loc[dat['word'] == word, 'def'].values[0]
+    except:
+        def_word = '단어의 뜻을 찾을 수 없습니다.'
+
+    output.insert(END, def_word)
 
 
 dat2 = pd.read_csv('dic_csv.csv')
@@ -30,11 +37,14 @@ label.grid(row = 0, column = 0, sticky = W)
 entry = Entry(window, width = 15, bg = 'light green')
 entry.grid(row = 1, column = 0, sticky = W)
 
-btn = Button(window, text='제출', width = 5, command = click)
+btn = Button(window, text='검색', width = 5, command = click)
 btn.grid(row = 2, column = 0, sticky = W)
 
 label2 = Label(window, text = '\n의미')
 label2.grid(row = 3, column = 0, sticky = W)
+
+output = Text(window, width=50, height=6, wrap=WORD, background='light green')
+output.grid(row=4, column=0, columnspan=2, sticky=W)
 
 
 window.mainloop()
